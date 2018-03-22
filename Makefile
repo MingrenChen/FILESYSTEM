5ae214ad1da98260457b5c0bb9d166e58b448f64
MingrenChen <chenmr9769@gmail.com>
2018-03-22 12:28:13 -0400

    init

---
+++ Makefile
@@ -0,0 +1,14 @@
+CFlag = -Wall -g -std=gnu99
+
+fs : ext2_cp.o ext2_utils.o
+	gcc $(CFlag) -o fs $^
+
+cp : ext2_cp.o ext2_utils.o
+	gcc $(CFlag) -o cp $^
+
+%.o : %.c ext2_utils.h ext2.h
+	gcc $(CFlag) -c $<
+
+clean : 
+	rm -f *.o traffic *~
+
---
+++ ext2_cp.c
@@ -0,0 +1,52 @@
+//
+// Created by LiAllen on 2018-03-19.
+//
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include "ext2_utils.h"
+
+
+unsigned char* disk;
+
+
+int main(int argc, char **argv) {
+    if(argc != 4) {
+        fprintf(stderr, "Usage: %s <image file name> <src file path> <abs path of file sys>\n", argv[0]);
+        exit(1);
+    }
+
+    /**
+    TODO: check if it's absolute path
+    **/
+    // if (argv[1][3]!='/') {
+    //     fprintf(stderr,"not abs path");
+    //     exit(1);
+    // }
+
+
+    init_ptrs(argv[1]);
+
+    inode_table;
+
+    // check if disk has enough space.
+    if (sb->s_free_blocks_count == 0 || sb->s_free_inodes_count == 0){
+        perror("no enough space");
+        exit(ENOMEM);
+    }
+
+    // open src file and get it's Size.
+    FILE* src = fopen(argv[2], "rb");
+	  if (src==NULL) {
+		    fprintf(stderr,"ENOENT:%s\n",strerror(ENOENT));
+		    exit(1);
+	  }
+    fseek (src, 0, SEEK_END);
+    int fileSize = ftell(src);
+    fseek(src, 0, SEEK_SET);
+
+    // find root dir
+
+
+
+}
